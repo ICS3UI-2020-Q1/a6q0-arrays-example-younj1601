649544b072fef4d8fe162f5007fff53aa4cf61dc
younj1601 <younj1601@wrdsb.ca>
2020-10-08 16:46:12 +0000

    Finished example for array

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *Array example
+ * @author Jackson
  */
 public class Main {
 
@@ -9,7 +10,28 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+   //scanner for input
+   Scanner input = new Scanner(System.in);
+   //variable type[] name = new variable_type[size]
+
+   //create an array for grades to be stored
+   double[] grades = new double[5];
+
+   //get grades from user and store them in the array
+   for(int i = 0; i < grades.length; i++){
+     System.out.println("Please enter grade " + (i + 1));
+     grades[i] = input.nextDouble();
+   }
+    double sum = 0;
     
+    //add up all of the grades into sum
+    for(int i = 0; i < grades.length; i++){
+      sum = sum + grades[i];
+    }
+
+    //determine the average age and let the user know
+    double average = sum/grades.length;
+    System.out.println("Your average grade is " + average);
+
   }
 }
